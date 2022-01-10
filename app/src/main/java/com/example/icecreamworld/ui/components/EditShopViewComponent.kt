6a0e0d369dae1ee17a89bb135d24546a8c97727e
PythSomeone <40042814+PythSomeone@users.noreply.github.com>
2022-01-10 16:25:19 +0100

    fixed scroll and background

--- app/src/main/java/com/example/icecreamworld/ui/components/EditShopViewComponent.kt
+++ app/src/main/java/com/example/icecreamworld/ui/components/EditShopViewComponent.kt
@@ -9,12 +9,9 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -34,6 +31,7 @@ import com.example.icecreamworld.model.Product
 import com.example.icecreamworld.model.Shop
 import com.example.icecreamworld.model.Tag
 import com.example.icecreamworld.ui.outlinedtextfields.InputTextField
+import com.example.icecreamworld.ui.theme.BackgroundColor
 import com.example.icecreamworld.ui.theme.ButtonBrown
 import com.example.icecreamworld.ui.theme.OutlineBrown
 import com.example.icecreamworld.viewmodel.ShopViewModel
@@ -75,169 +73,169 @@ fun ShopFormSection(
     }
     val context = LocalContext.current
     val scrollState = rememberScrollState()
-    Column(modifier = modifier.fillMaxWidth()) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
+    ) {
+        Column(
+            modifier = modifier
                 .fillMaxWidth()
-                .padding(horizontal = 20.dp)
-                .padding(horizontal = 20.dp)
                 .verticalScroll(state = scrollState)
         ) {
 
-            Spacer(modifier = Modifier.width(16.dp))
-        }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                content = {
+                    if (shopId != null) {
+                        EditShopTextBox()
+                    } else {
+                        AddShopTextBox()
+                    }
 
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            content = {
-                if(shopId!=null) {
-                    EditShopTextBox()
-                }
-                else{
-                    AddShopTextBox()
-                }
+                    Spacer(Modifier.height(20.dp))
 
-                Spacer(Modifier.height(20.dp))
-                if ((imageUri != null)) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(150.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        content = {
-                            imageUri?.let {
-                                if (Build.VERSION.SDK_INT < 28) {
-                                    bitmap.value = MediaStore.Images
-                                        .Media.getBitmap(context.contentResolver, it)
+                    if ((imageUri != null)) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(150.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            content = {
+                                imageUri?.let {
+                                    if (Build.VERSION.SDK_INT < 28) {
+                                        bitmap.value = MediaStore.Images
+                                            .Media.getBitmap(context.contentResolver, it)
 
-                                } else {
-                                    val source = ImageDecoder
-                                        .createSource(context.contentResolver, it)
-                                    bitmap.value = ImageDecoder.decodeBitmap(source)
-                                }
-                                bitmap.value?.let { btm ->
-                                    ShopImage(
-                                        btm,
-                                        modifier = modifier
-                                            .aspectRatio(1f, matchHeightConstraintsFirst = true)
-                                            .padding(3.dp)
-                                            .height(100.dp)
-                                    )
+                                    } else {
+                                        val source = ImageDecoder
+                                            .createSource(context.contentResolver, it)
+                                        bitmap.value = ImageDecoder.decodeBitmap(source)
+                                    }
+                                    bitmap.value?.let { btm ->
+                                        ShopImage(
+                                            btm,
+                                            modifier = modifier
+                                                .aspectRatio(1f, matchHeightConstraintsFirst = true)
+                                                .height(100.dp)
+                                        )
+                                    }
                                 }
                             }
-                        }
-                    )
-                } else {
-                    Column(
+                        )
+                    } else {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(150.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            content = {
+                                Image(
+                                    painter = rememberImagePainter(shop?.image),
+                                    contentDescription = null,
+                                    modifier = modifier
+                                        .aspectRatio(1f, matchHeightConstraintsFirst = true)
+                                        .height(100.dp)
+                                )
+                            }
+                        )
+
+
+                    }
+
+
+                    Spacer(Modifier.height(20.dp))
+
+                    Button(
+                        onClick = {
+                            launcher.launch("image/*")
+                        },
+                        shape = RoundedCornerShape(6.dp),
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .height(150.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
+                            .width(200.dp),
                         content = {
-                            Image(
-                                painter = rememberImagePainter(shop?.image),
-                                contentDescription = null,
-                                modifier = modifier
-                                    .aspectRatio(1f, matchHeightConstraintsFirst = true)
-                                    .padding(3.dp)
-                                    .height(100.dp)
+                            Text(
+                                text = "Choose photo",
+                                color = Color.White
                             )
-                        }
+                        },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = ButtonBrown)
+                    )
+
+                    Spacer(Modifier.height(20.dp))
+
+                    InputTextField(label = "Change name", name = name)
+                    InputTextField(label = "Change description", name = description)
+                    InputTextField(label = "Change location", name = location)
+                    InputTextField(label = "Change website address", name = websiteLink)
+
+                    Spacer(Modifier.height(20.dp))
+
+                    Button(
+                        onClick = {
+                            /*TODO*/
+                        },
+                        shape = RoundedCornerShape(6.dp),
+                        modifier = Modifier
+                            .width(200.dp),
+                        content = {
+                            Text(
+                                text = "Change menu",
+                                color = Color.White
+                            )
+                        },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = ButtonBrown)
+                    )
+
+                    Spacer(Modifier.height(20.dp))
+
+                    Button(
+                        onClick = {
+                            Toast.makeText(
+                                context,
+                                "Changes submitted...",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            val shopToSubmit = Shop(
+                                name = name.value,
+                                description = description.value,
+                                location = location.value,
+                                websiteLink = websiteLink.value,
+                                menu = menu.value
+                            )
+
+                            viewModel.sendForm(
+                                shop = shopToSubmit,
+                                toChange = shopId,
+                                uri = imageUri
+                            )
+
+                            if (shopId != null) {
+                                navController.navigate("Shop/${shopId}")
+                            } else {
+                                navController.navigate("HomePage")
+                            }
+
+                        },
+                        shape = RoundedCornerShape(6.dp),
+                        modifier = Modifier
+                            .width(200.dp),
+                        content = {
+                            Text(
+                                text = "Save",
+                                color = Color.White,
+                            )
+                        },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = ButtonBrown)
                     )
 
 
                 }
+            )
 
-
-                Spacer(Modifier.height(20.dp))
-
-                Button(
-                    onClick = {
-                        launcher.launch("image/*")
-                    },
-                    shape = RoundedCornerShape(6.dp),
-                    modifier = Modifier
-                        .width(200.dp),
-                    content = {
-                        Text(
-                            text = "Choose photo",
-                            color = Color.White
-                        )
-                    },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = ButtonBrown)
-                )
-
-                Spacer(Modifier.height(20.dp))
-
-                InputTextField(label = "Change name", name = name)
-                InputTextField(label = "Change description", name = description)
-                InputTextField(label = "Change location", name = location)
-                InputTextField(label = "Change website address", name = websiteLink)
-
-                Spacer(Modifier.height(20.dp))
-
-                Button(
-                    onClick = {
-                        /*TODO*/
-                    },
-                    shape = RoundedCornerShape(6.dp),
-                    modifier = Modifier
-                        .width(200.dp),
-                    content = {
-                        Text(
-                            text = "Change menu",
-                            color = Color.White
-                        )
-                    },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = ButtonBrown)
-                )
-
-                Spacer(Modifier.height(20.dp))
-
-                Button(
-                    onClick = {
-                        Toast.makeText(
-                            context,
-                            "Changes submitted...",
-                            Toast.LENGTH_SHORT
-                        ).show()
-
-                        val shopToSubmit = Shop(
-                            name=name.value,
-                            description = description.value,
-                            location = location.value,
-                            websiteLink = websiteLink.value,
-                            menu = menu.value
-                        )
-
-                        viewModel.sendForm(shop = shopToSubmit, toChange = shopId, uri = imageUri)
-
-                        if(shopId!=null) {
-                            navController.navigate("Shop/${shopId}")
-                        }
-                        else{
-                            navController.navigate("HomePage")
-                        }
-
-                    },
-                    shape = RoundedCornerShape(6.dp),
-                    modifier = Modifier
-                        .width(200.dp),
-                    content = {
-                        Text(
-                            text = "Save",
-                            color = Color.White,
-                        )
-                    },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = ButtonBrown)
-                )
-
-
-            }
-        )
-
+        }
     }
 }
 
