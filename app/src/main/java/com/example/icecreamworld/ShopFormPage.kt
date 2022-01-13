f6667247d5f4cf5fc257d8a75a1a3a4cff08a74c
ciuszek <49906488+ciuszek@users.noreply.github.com>
2022-01-13 21:05:37 +0100

    Added tags, menu to shop form. TODO: fix editing product.

--- app/src/main/java/com/example/icecreamworld/ShopFormPage.kt
+++ app/src/main/java/com/example/icecreamworld/ShopFormPage.kt
@@ -2,16 +2,14 @@ package com.example.icecreamworld
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.runtime.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.icecreamworld.ui.components.ShopFormSection
-import kotlinx.coroutines.delay
 
+@ExperimentalMaterialApi
 @ExperimentalFoundationApi
 @Composable
 fun ShopFormScreen(
